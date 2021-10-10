44062e07c9dc0ea3bcfe48e082cba26f401a807e
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-10 08:28:10 -0300

    Add files via upload

---
+++ oqueeoquee.py
@@ -0,0 +1,17 @@
+pergunta = str(input("complete o ditado.\n Quem não chora não ... ")).lower()
+
+
+if pergunta == 'mama':
+    print("Parabèns {}" .format(chr(9989)))
+
+else:
+    print('Que peninha, você errou {}' .format(chr(128532)))
+    
+    
+    
+    
+          
+          
+    
+    
+

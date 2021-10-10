14f3ecf62315a0bdd42cc40a1d48fc2dbe6dfc0a
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-10 08:27:19 -0300

    Add files via upload

---
+++ 4brincando.py
@@ -0,0 +1,26 @@
+print( '''Tesde de conhecimento.\n
+Qual é o maior osso do corpo humano? \n 
+a - estribo
+b - tibia
+c - fêmur''')
+brinc = input().lower()
+
+
+if brinc == 'a':
+    print('Esse é o menor osso do corpo humano :( ')
+    
+    
+
+elif brinc == 'b':
+    print('Foi por pouco, esse é o segundo maior osso :( ')
+    
+    
+
+elif brinc == 'c':
+    print('Correto!! :) ')
+    
+
+else:
+    ('Você não escolheu a, b ou c :( ')
+
+

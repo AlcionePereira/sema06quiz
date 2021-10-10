c26923f8db40c178ead3170ae04460135d603624
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-10-10 08:29:13 -0300

    Add files via upload

---
+++ upper.py
@@ -0,0 +1,12 @@
+resposta = input("No Python, como se chama uma 'caixa' usada para dados? ").upper()
+
+
+if resposta == 'VARIAVEL':
+    print(" :) " * 100)
+
+else:
+    print(" :( " * 100)
+
+
+print("Obrigada por jogar!")
+    
